f75eaf3b31ef0243486ba5a4389b2e29c6731f71
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2024-11-21 20:37:04 +0200

    refactor: general cleanup.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Navbar from "./components/navbar";
-import Styles from "./styles.css"
 
 export default function Home() {
-  return (
-    <Navbar/>
-  )    
-
+  return <Navbar />;
 }
