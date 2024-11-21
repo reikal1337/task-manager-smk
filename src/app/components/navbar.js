ba40c59b5eab2e344cbc567e03f77a25b2b5ea32
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2024-11-21 20:38:54 +0200

    fix: applying styles in Navbar.

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,13 +1,19 @@
-export default function Navbar(){
-    return <nav className="navbar">
-        <a href="./" className="pagrindinis" >TaskMate</a>
-        <ul>
-            <li>
-                <a href="/prisijungti">Prisijungti</a>
-            </li>
-            <li>
-                <a href="/registracija">Pradėti</a>
-            </li>
-        </ul>
+import "../styles.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="./" className="pagrindinis">
+        TaskMate
+      </a>
+      <ul>
+        <li>
+          <a href="/prisijungti">Prisijungti</a>
+        </li>
+        <li>
+          <a href="/registracija">Pradėti</a>
+        </li>
+      </ul>
     </nav>
+  );
 }
