5a6546f2b67a44d8b0ca6ee43be93d1994347654
dovydasqq <dovydasbiriukas@gmail.com>
2024-11-30 23:13:55 +0200

    added useState to registration.js

--- src/app/components/registration.js
+++ src/app/components/registration.js
@@ -1,19 +1,49 @@
+"use client";
+import { useState } from "react";
 import "../registration.css";
 
 export default function Registration() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Data Submitted:", { email, password });
+    };
+
     return (
         <div className="container">
-            <form className="registration-form">
+            <form className="registration-form" onSubmit={handleSubmit}>
                 <h1>Registracija</h1>
                 
                 <label htmlFor="email">El. Paštas</label>
-                <input type="text" id="email" placeholder="Įveskite el. paštą" />
+                <input
+                    type="text"
+                    id="email"
+                    placeholder="Įveskite el. paštą"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
                 
                 <label htmlFor="password">Slaptažodis</label>
-                <input type="password" id="password" placeholder="Įveskite slaptažodį" />
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Įveskite slaptažodį"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 
                 <label htmlFor="confirm-password">Pakartokite Slaptažodį</label>
-                <input type="password" id="confirm-password" placeholder="Pakartokite slaptažodį" />
+                <input
+                    type="password"
+                    id="confirm-password"
+                    placeholder="Pakartokite slaptažodį"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
                 
                 <button type="submit" className="btn">Registruotis</button>
                 
