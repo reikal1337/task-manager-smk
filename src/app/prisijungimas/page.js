6b82882ca1fdd15104ca8c42cac6d42f41f97387
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2025-01-12 16:31:15 +0200

    feat: updated prisijungimas page.

--- src/app/prisijungimas/page.js
+++ src/app/prisijungimas/page.js
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import "./prisijungimas.css";
 
 const PrisijungimasPage = () => {
-  return <h1>Prisijungimas</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data Submitted:", { email, password });
+  };
+
+  return (
+    <div className="container">
+      <form className="registration-form" onSubmit={handleSubmit}>
+        <h1>Prisijungimas</h1>
+
+        <label htmlFor="email">El. Paštas</label>
+        <input
+          type="text"
+          id="email"
+          placeholder="Įveskite el. paštą"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Slaptažodis</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Įveskite slaptažodį"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit" className="btn">
+          Prisjungti
+        </button>
+
+        <h2>Arba</h2>
+
+        <button type="button" className="socialbtn google">
+          Prisijunkite su Google
+        </button>
+        <button type="button" className="socialbtn facebook">
+          Prisijunkite su Facebook
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default PrisijungimasPage;
